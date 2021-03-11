ce193a4d90f77f112be350753e894f0ad1f5f7f4
62011213 Pitchakorn Pichetnaruemit <62011213@kmitl.ac.th>
2021-03-11 17:14:27 +0700

    Update Simple_drawing_window.py

--- Simple_drawing_window.py
+++ Simple_drawing_window.py
@@ -8,7 +8,7 @@ class Simple_drawing_window(QWidget):
     def __init__(self):
         QWidget.__init__(self, None)
         self.setWindowTitle("Simple Drawing")
-        self.rabbit = QPixmap("image/rabbit.png")
+        self.rabbit = QPixmap("rabbit.png")
 
     def paintEvent(self, e):
         p = QPainter()
