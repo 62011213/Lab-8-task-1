8ca14778dde910c9553ee2731c30b3aa2cea5eb0
62011213 Pitchakorn Pichetnaruemit <62011213@kmitl.ac.th>
2021-03-11 17:29:36 +0700

    Update drawing_window3.py

--- drawing_window3.py
+++ drawing_window3.py
@@ -14,8 +14,8 @@ class Simple_drawing_window3(Simple_drawing_window):
         p = QPainter()
         p.begin(self)
 
-        # p.setPen(QColor(0, 0, 0))
-        # p.setBrush(QColor(0, 127, 0))
+        p.setPen(QColor(0, 0, 0))
+        p.setBrush(QColor(255, 127, 0))
         # p.drawPolygon([
         #     QPoint( 70, 100 ), QPoint( 100, 110 ),
         #     QPoint( 130, 100 ), QPoint( 100, 150 ),
@@ -30,8 +30,8 @@ class Simple_drawing_window3(Simple_drawing_window):
         # )
 
         p.drawPolygon([
-            QPoint( 50, 100 ), QPoint( 100, 100 ),
-            QPoint( 50, 50 ), QPoint( 100, 50 )
+            QPoint( 50, 400 ), QPoint( 130, 400 ),
+            QPoint( 50, 100 ), QPoint( 130, 100 )
         ])
 
         p.drawPixmap(QRect( 200, 100, 320, 320 ), self.rabbit )
